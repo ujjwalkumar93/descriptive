6205c66a9b40f528a55db0ecb240ecd6b6b5de8e
ujjwalkumar93 <pathakujjwal93@gmail.com>
2022-01-25 17:15:02 +0530

    updated

---
+++ descriptive/www/home.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+no_cache = 1
+
+def get_context(context):
+    pass
