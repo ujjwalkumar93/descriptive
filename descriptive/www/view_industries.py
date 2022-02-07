101056231353752001472f4836f71b1cbba389de
ujjwalkumar93 <pathakujjwal93@gmail.com>
2022-02-08 00:02:51 +0530

    fixed route

--- descriptive/www/view_industries.py
+++ descriptive/www/view_industries.py
@@ -2,4 +2,4 @@ import frappe
 
 def get_context(contex):
     query_params = frappe.request.environ.get('QUERY_STRING')
-    contex.data = frappe.get_doc("Industries",query_params)
+    contex.data = frappe.get_doc("Industries",{"industry_name" : query_params})
--- descriptive/www/view_insight.py
+++ descriptive/www/view_insight.py
@@ -2,4 +2,4 @@ import frappe
 
 def get_context(contex):
     query_params = frappe.request.environ.get('QUERY_STRING')
-    contex.data = frappe.get_doc("Insights",query_params)
+    contex.data = frappe.get_doc("Insights",{"insight_name": query_params})
