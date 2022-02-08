b8ccfbd9e29edd262d25cf6ad2980204e8eff14f
ujjwalkumar93 <pathakujjwal93@gmail.com>
2022-02-09 03:03:50 +0530

    updated

--- descriptive/templates/pages/industry.py
+++ descriptive/templates/pages/industry.py
@@ -2,4 +2,4 @@ import frappe
 
 def get_context(contex):
     query_params = frappe.form_dict.name
-    contex.data = frappe.get_doc("Industries",{"industry_name" : query_params})
+    contex.data = frappe.get_doc("Industries",{"name" : query_params})
