83804aa6bcf05f7e2da208b91377278b7c8ba95a
ujjwalkumar93 <pathakujjwal93@gmail.com>
2022-02-10 01:01:49 +0530

    made change in css

--- descriptive/templates/pages/insight.py
+++ descriptive/templates/pages/insight.py
@@ -2,5 +2,6 @@ import frappe
 
 def get_context(contex):
     query_params = frappe.form_dict.name
+    #frappe.throw(frappe.form_dict.name)
     # query_params = frappe.request.environ.get('QUERY_STRING')
     contex.data = frappe.get_doc("Insights",{"insight_name": query_params})
