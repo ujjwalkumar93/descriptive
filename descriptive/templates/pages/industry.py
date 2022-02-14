455eb1772f1b3012149ff0a97e5e8f64326d92a5
ujjwalkumar93 <pathakujjwal93@gmail.com>
2022-02-15 00:01:09 +0530

    industry list in industry view updated

--- descriptive/templates/pages/industry.py
+++ descriptive/templates/pages/industry.py
@@ -3,3 +3,4 @@ import frappe
 def get_context(contex):
     query_params = frappe.form_dict.name
     contex.data = frappe.get_doc("Industries",{"industry_name" : query_params})
+    contex.all_industies = frappe.get_all("Industries",['*'])
